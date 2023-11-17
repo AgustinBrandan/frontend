f40176eeb88bdee911be17f8fb4699b33d6de3f7
AgustinBrandan <agustinbrandan97@gmail.com>
2023-11-17 11:29:39 -0300

    Proyecto listo

--- src/context/CarritoProvider.jsx
+++ src/context/CarritoProvider.jsx
@@ -5,7 +5,7 @@ export const CarritoProvider = ({ children }) => {
   const [carrito, setCarrito] = useState();
 
   
-  const cargarCarrito = async () => {
+  const crearCarrito = async () => {
     try {
         const response = await fetch(`http://localhost:5000/api/carrito`, {
             method: 'POST',
@@ -14,23 +14,42 @@ export const CarritoProvider = ({ children }) => {
             },
           });
       const data = await response.json(); 
-    //   console.log(data._id)
       setCarrito(data);
     } catch (error) {
       console.error('Error al cargar el carrito:', error);
     }
   };
 
-  useEffect(() => {
-    return () => {
-      cargarCarrito()
+  const productosCarrito = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/api/carrito/${carrito._id}/productos`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      const data = await response.json();
+      setCarrito(data); // Ajusta el nombre de la propiedad según la respuesta real
+    } catch (error) {
+      console.error(error);
     }
-  }, [])
+  };
+  useEffect(() => {
+    const cargarDatos = async () => {
+      try {
+        await crearCarrito();
+        await productosCarrito();
+      } catch (error) {
+        console.error('Error al cargar los datos:', error);
+      }
+    };
+  
+    cargarDatos();
+  }, []);
+
 
   const agregarProducto = async (productoId, cantidad) => {
     try {
-        console.log("CARRITO: ",carrito._id)
-        console.log("Producto: ",productoId)
       const response = await fetch(`http://localhost:5000/api/carrito/${carrito._id}/productos`, {
         method: 'POST',
         headers: {
@@ -38,15 +57,61 @@ export const CarritoProvider = ({ children }) => {
         },
         body: JSON.stringify({ productoId, cantidad }),
       });
-
-      const carritoActualizado = await response.json();
-      console.log(carritoActualizado)
-      setCarrito(carritoActualizado)
+      //Como tengo el get no necesito setear el carrito
+      // const carritoActualizado = await response.json();
+      // setCarrito(carritoActualizado)
+      
     } catch (error) {
       console.error('Error al agregar el producto al carrito:', error);
     }
+    productosCarrito()
   };
 
+  const actualizarProducto = async (productoId, nuevaCantidad) => {
+    try {
+      await fetch(`http://localhost:5000/api/carrito/${carrito._id}/producto/${productoId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ cantidad: nuevaCantidad }), 
+      });
+
+    } catch (error) {
+    }
+    productosCarrito()
+  };
+
+  const eliminarProducto = async (productoId) => {
+    try {
+      await fetch(`http://localhost:5000/api/carrito/${carrito._id}/producto/${productoId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+    } catch (error) {
+      console.error('Error al eliminar el producto del carrito:', error);
+    }
+    productosCarrito()
+  };
+
+  const eliminarCarrito = async () => {
+    try {
+
+       await fetch(`http://localhost:5000/api/carrito/${carrito._id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+    } catch (error) {
+      console.error('Error al eliminar el producto del carrito:', error);
+    }
+    productosCarrito()
+  };
   
 
   return (
@@ -54,6 +119,9 @@ export const CarritoProvider = ({ children }) => {
       value={{
         carrito,
         agregarProducto,
+        actualizarProducto,
+        eliminarProducto,
+        eliminarCarrito
       }}
     >
       {children}
--- src/pages/CarritoPage.jsx
+++ src/pages/CarritoPage.jsx
@@ -1,43 +1,73 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CarritoContext } from "../context/CarritoContext";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/CarritoPage.css";
 
 export const CarritoPage = () => {
-  const { carrito } = useContext(CarritoContext);
+  const { carrito, eliminarProducto, eliminarCarrito, actualizarProducto } =
+    useContext(CarritoContext); // Agrega la función eliminarProducto
 
   if (!carrito.productos || carrito.productos.length === 0) {
     return <p>Carrito Vacío.</p>;
   }
-    // Verificar el contenido del carrito y sus productos
-    console.log("CARRITO desde CARRITO:", carrito);
+
+  const handleEliminarProducto = (productoId) => {
+    eliminarProducto(productoId);
+  };
+
+  const handleEliminarCarrito = () => {
+    eliminarCarrito();
+  };
+
+  const BtnEliminarProducto = (productoId, cantidadActualProducto) => {
+    const nuevaCantidad = cantidadActualProducto - 1;
+    actualizarProducto(productoId, nuevaCantidad);
+  };
+  
+  const BtnAgregarProducto = (productoId, cantidadActualProducto) => {
+    const nuevaCantidad = cantidadActualProducto + 1;
+    actualizarProducto(productoId, nuevaCantidad);
+  };
 
   return (
     <>
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">ID</th>
             <th scope="col">Nombre</th>
             <th scope="col">Precio</th>
             <th scope="col">Cantidad</th>
+            <th scope="col">Total</th>
+            <th scope="col">Quitar/Agregar</th>
             <th scope="col">Eliminar</th>
           </tr>
         </thead>
         <tbody>
           {carrito.productos.map((producto) => (
             <tr key={producto._id}>
-              <td>{producto._id}</td>
-              <td>{producto.nombre}</td>
-              <td>{producto.precio}</td>
+              <td>{producto.nombreProducto}</td>
+              <td>{producto.precioUnitario}</td>
               <td>{producto.cantidad}</td>
+              <td>{producto.precioTotalProducto}</td>
               <td>
-                <button className="btn btn-outline-primary">-</button>
-                <button className="btn btn-outline-primary">+</button>
+                <button
+                  className="btn btn-outline-primary"
+                  onClick={() => BtnEliminarProducto(producto.producto,producto.cantidad)}
+                >
+                  -
+                </button>
+                <button
+                  className="btn btn-outline-primary"
+                  onClick={() => BtnAgregarProducto(producto.producto,producto.cantidad)}
+                >
+                  +
+                </button>
               </td>
               <td>
                 <button
                   type="button"
                   className="btn btn-danger"
-                  // onClick={() => eliminarCompra(producto._id)}
+                  onClick={() => handleEliminarProducto(producto.producto)}
                 >
                   Eliminar
                 </button>
@@ -54,6 +84,13 @@ export const CarritoPage = () => {
           disabled={carrito.productos.length < 1}
         >
           COMPRAR
+        </button>
+        <button
+          className="btn btn-danger"
+          onClick={() => handleEliminarCarrito()}
+          disabled={carrito.productos.length === 0}
+        >
+          Eliminar Carrito
         </button>
       </div>
     </>
